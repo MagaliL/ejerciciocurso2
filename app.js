3ecba0342b40c7298be835c6c5570e7d18513a18
Magali L. Espinosa <magalizethe@gmail.com>
2025-08-17 17:57:11 -0600

    Se agregó una función para que limpie la caja de input cada vez que se intenta un nuevo número y se activó el botón de iniciar un nuevo juego una vez se acierte el número secreto, este botón aún no tiene funcionalidad

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-
-// let parrafo = document.querySelector('p');
-// parrafo.innerHTML= 'Indica un número del 1 al 10';
-
 let numeroSecreto = generarNumeroSecreto();
 //A la declaración de esta variable se le puede asignar la función que creamos abajo para que se pueda utilizar de forma global. 
 let intentos = 1; //En esta parte del código se declara una variable de intentos para la implementación de un contador, es igual a uno porque siempre va a intentarlo al menos una vez. 
 
 //Revisamos que ejecute como debería. El resultado deberá ser un número entero de 1 a 10
-console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento, texto){
 
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML= texto;
     return;
+    //En esta función se asigna el texto a un elemento del DOM, se utiliza el método querySelector para seleccionar el elemento y luego se asigna el texto a su propiedad innerHTML. document es un objeto que representa el documento HTML cargado en el navegador, y querySelector es un método que permite seleccionar el primer elemento que coincida con el selector CSS proporcionado. 
+    //elementoHTML es una variable que almacena el elemento HTML seleccionado, y la propiedad innerHTML se utiliza para establecer o obtener el contenido HTML de un elemento.
 };
 
 function verificarIntento(){
@@ -36,6 +34,7 @@ if (numeroUsuario === numeroSecreto){
 
         intentos ++;
         //Este es el contador que va a ir aumentando cuando el usuario no acierte al número secreto. 
+        limpiarcaja(); 
    }
    ;
    //Esta parte del código es para verificar que el numeroUsuario y el numeroSecreto sean iguales tanto en valor como en tipo, ya que el número secreto es un número entero y el número del usuario también debe serlo para que la comparación sea correcta. El tipo de dato que va a regresar es booleano. 
@@ -49,8 +48,15 @@ function generarNumeroSecreto(){
     //Esta función va a hacer la operación necesaria para crear al número secreto, la función se puede asignar a una variable para que tenga alcance global, si se pone aquí la variable sólo tendrá alcance de bloque y no podrá ser utilizada fuera de la función. 
 }
 
-// En esta función se esta pasando los parámetros elemento y para que la función pueda trabajar con ellas como variables 
+function limpiarcaja(){
+    document.querySelector('#valorUsuario').value = "";
+    
 
+    //Esta función se encarga de limpiar el input del usuario, se utiliza el método querySelector para seleccionar el elemento con el id valorUsuario y luego se establece su valor a una cadena vacía.
+}
+
+
+// En esta parte se esta pasando los parámetros elemento y para que la función pueda trabajar con ellas como variables 
 
 asignarTextoElemento('h1','Juego del Número Secreto');
 asignarTextoElemento('p','Indica un número entre 1 y 10');
